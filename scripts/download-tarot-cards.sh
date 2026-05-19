7aa9083d737f0b14e5835b0cbc866f02c5f688b8
MinjiJeon <minji799@gmail.com>
2026-05-19 21:13:49 +0900

    fix: correct stale wikimedia URLs and macOS grep compat in download script

--- scripts/download-tarot-cards.sh
+++ scripts/download-tarot-cards.sh
@@ -12,7 +12,7 @@ download() {
     echo "Downloading $1..."
     curl -sL --retry 3 "$url" -o "$dest"
     # Verify it's actually an image (not a 404 HTML page)
-    if ! file "$dest" | grep -q "JPEG\|image"; then
+    if ! file "$dest" | grep -qE "JPEG|image"; then
       echo "WARNING: $1 may not be a valid image. Check manually."
     fi
   else
@@ -21,12 +21,12 @@ download() {
 }
 
 download "00-fool.jpg"             "9/90/RWS_Tarot_00_Fool.jpg"
-download "01-magician.jpg"         "1/10/RWS_Tarot_01_Magician.jpg"
+download "01-magician.jpg"         "d/de/RWS_Tarot_01_Magician.jpg"
 download "02-high-priestess.jpg"   "8/88/RWS_Tarot_02_High_Priestess.jpg"
 download "03-empress.jpg"          "d/d2/RWS_Tarot_03_Empress.jpg"
 download "04-emperor.jpg"          "c/c3/RWS_Tarot_04_Emperor.jpg"
 download "05-hierophant.jpg"       "8/8d/RWS_Tarot_05_Hierophant.jpg"
-download "06-lovers.jpg"           "3/3a/RWS_Tarot_06_Lovers.jpg"
+download "06-lovers.jpg"           "d/db/RWS_Tarot_06_Lovers.jpg"
 download "07-chariot.jpg"          "9/9b/RWS_Tarot_07_Chariot.jpg"
 download "08-strength.jpg"         "f/f5/RWS_Tarot_08_Strength.jpg"
 download "09-hermit.jpg"           "4/4d/RWS_Tarot_09_Hermit.jpg"
